9815d2723025c74acf123364025239c9be5e9f04
maochengfangv <maochengfangv@gmail.com>
2026-07-01 18:58:02 +0800

    feat:flutter demo

---
+++ FlutterContainerDemo/FlutterContainerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlutterContainerDemo
+//
+//  Created by maochengfang on 2026/7/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlutterContainerDemo/FlutterContainerDemoUITests/FlutterContainerDemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FlutterContainerDemoUITestsLaunchTests.swift
+//  FlutterContainerDemoUITests
+//
+//  Created by maochengfang on 2026/7/1.
+//
+
+import XCTest
+
+final class FlutterContainerDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
